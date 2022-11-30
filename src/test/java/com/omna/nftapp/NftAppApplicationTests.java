6722ff358e5fc2209885ef58aa5982c88a0794ed
Ítalo Moura <italo.moura@escolaconquer.com.br>
2022-11-30 20:57:58 -0300

    feat: adicionando disabled no test

--- src/test/java/com/omna/nftapp/NftAppApplicationTests.java
+++ src/test/java/com/omna/nftapp/NftAppApplicationTests.java
@@ -1,29 +1,17 @@
 package com.omna.nftapp;
 
-import com.omna.nftapp.integration.NFTs.v1.dto.NFTDTO;
-import com.omna.nftapp.resource.v1.NFTResource;
-import com.omna.nftapp.service.NFTService;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-
-import java.util.Collections;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
+@Disabled
 @AutoConfigureTestDatabase
-@SpringBootTest
+@SpringBootTest(classes = NftAppApplication.class)
 class NftAppApplicationTests {
 
 	@Autowired
